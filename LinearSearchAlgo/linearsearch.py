2a6231f9f94788245ac13a1f4b12500edf70ffd5
deep <deepaksingodiya2000@gmail.com>
2022-03-14 15:44:06 +0530

    my improvised code for linear search that also print the position no.

--- LinearSearchAlgo/linearsearch.py
+++ LinearSearchAlgo/linearsearch.py
@@ -1,10 +1,9 @@
 car = [1,8,3,5,4,9]
 CarNo = int(input("Enter the car no. to search: "))
 def mycarsearch_algo(cars, carno):
-    for i in cars:
-        if i == carno:
-            print("found")
+    for i in list(range(len(cars))):
+        if cars[i] == carno:
+            print("found at position", i)
             break
-        else:
-            print("notfound")
+       
 mycarsearch_algo(car, CarNo)
